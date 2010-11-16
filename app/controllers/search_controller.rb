3a386429782cfae16f6ae59291088b05cd8f578c
Dimitri Kurashvili <dimitri@MacBook-Air.local>
2010-11-16 12:07:34 +0400

    numberation in move form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,47 +1,40 @@
-# ძებნის კლასი
 class Search
-  # ძებნის ატრიბუტები
-  attr_accessor :name, :direqcia_id, :nomenclature_id
+  attr_accessor :name, :direqcia_id, :nomenclature_id, :book_year, :enter_year
 
-  # ძებნის ობიექტის შექმნა პარამეტრების რუკიდან
   def self.create(map)
     s = Search.new
     s.name = map[:name]
     s.direqcia_id = map[:direqcia_id] ? map[:direqcia_id].to_i : 0
     s.nomenclature_id = map[:nomenclature_id] ? map[:nomenclature_id].to_i : 0
+    s.book_year = map[:book_year] ? map[:book_year] : nil
+    s.enter_year = map[:enter_year] ? map[:enter_year] : nil
     s
   end
 
-  # ცარიელია თუ არა ეს ძებნა?
   def empty?
-    if (name.nil? or name.empty?) and (direqcia_id  == 0) and (nomenclature_id == 0)
-      return false
-    else
-      return true
-    end
+    (self.name.nil? or self.name.empty?) and (self.direqcia_id == 0) and (self.nomenclature_id == 0) and
+    (self.book_year.nil? or self.book_year.empty?) and (self.enter_year.nil? or self.enter_year.empty?)
   end
 end
 
-# ძებნის კონტროლერი
 class SearchController < ApplicationController
 
   def index
     @title = 'წიგნების ძებნა'
-    
     if params[:search]
-      @search = Search.create(params[:search])
-      unless @search.empty?
+      @search = Search.create(params[:search] ? params[:search] : {})
+      if @search.empty?
         flash.now[:notice] = 'განსაზღვრეთ ძებნის პარამეტრები'
         @books = []
       else
         where = ''
         where_params = {}
-        # სახელის დამატება
+        # name
         unless @search.name.nil? or @search.name.empty?
           where += 'books.name like :name'
           where_params[:name] = '%' + @search.name + "%"
         end
-        # დირექციის დამატება
+        # direqcia
         unless @search.direqcia_id == 0
           unless where.empty?
             where += ' and '
@@ -49,7 +42,7 @@ class SearchController < ApplicationController
           where += 'nomenclatures.direqcia_id = :direqcia_id'
           where_params[:direqcia_id] = @search.direqcia_id
         end
-        # ნომენკლატურის დამატება
+        # nomenclature
         unless @search.nomenclature_id == 0
           unless where.empty?
             where += ' and '
@@ -57,7 +50,23 @@ class SearchController < ApplicationController
           where += 'nomenclature_id = :nomenclature_id'
           where_params[:nomenclature_id] = @search.nomenclature_id
         end
-        # წიგნების მიღება
+        # book_year
+        unless @search.book_year.nil? or @search.book_year.empty?
+          unless where.empty?
+            where += ' and '
+          end
+          where += 'book_year = :book_year'
+          where_params[:book_year] = @search.book_year
+        end
+        # enter_year
+        unless @search.enter_year.nil? or @search.enter_year.empty?
+          unless where.empty?
+            where += ' and '
+          end
+          where += 'enter_year = :enter_year'
+          where_params[:enter_year] = @search.enter_year
+        end
+        # get books
         #@books = Book.all(:conditions => [where, where_params], :include => :nomenclature)
         @books = Book.paginate :page => params[:page], :conditions => [where, where_params], :include => :nomenclature, :order => 'books.name'
       end
